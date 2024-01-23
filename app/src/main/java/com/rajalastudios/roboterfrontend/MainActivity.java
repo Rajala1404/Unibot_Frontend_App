1a6b11538e52d50b979d0bb1e5584e0f2e59aa9c
Rajala1404 <post.rajan@schirmer-family.de>
2024-01-23 15:06:03 +0100

    Initial commit

---
+++ app/src/main/java/com/rajalastudios/roboterfrontend/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rajalastudios.roboterfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
