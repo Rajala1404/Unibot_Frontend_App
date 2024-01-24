ee09bf681f54a403e70d73b5074b3ada2bae4299
Rajala1404 <post.rajan@schirmer-family.de>
2024-01-24 15:00:07 +0100

    Initial Commit

--- app/src/main/java/com/rajalastudios/roboterfrontend/MainActivity.java
+++ app/src/main/java/com/rajalastudios/roboterfrontend/MainActivity.java
@@ -3,6 +3,8 @@ package com.rajalastudios.roboterfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button connect_button = (Button)findViewById(R.id.connect_button);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.connect_button: {
+                System.out.println("Test");
+            }
+        }
     }
 }
