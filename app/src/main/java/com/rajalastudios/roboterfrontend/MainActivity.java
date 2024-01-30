dcba63aa2ddd2ac787f3d2b2e40eccce2ddaad59
Rajala1404 <post.rajan@schirmer-family.de>
2024-01-30 06:39:18 +0100

    Things

--- app/src/main/java/com/rajalastudios/roboterfrontend/MainActivity.java
+++ app/src/main/java/com/rajalastudios/roboterfrontend/MainActivity.java
@@ -3,23 +3,45 @@ package com.rajalastudios.roboterfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private Button connectButton;
+
+    String ipAddress = "";
+    String port = "";
+
+    File file = new File("data.ludat");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button connect_button = (Button)findViewById(R.id.connect_button);
+
+        connectButton = findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Log.d("INFO", "Saving Values!");
+                saveValues();
+            }
+        });
     }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.connect_button: {
-                System.out.println("Test");
-            }
-        }
+    public void saveValues() {
+        EditText ipAddressTextBox = (EditText) findViewById(R.id.ipAddressTextBox);
+        EditText portTextBox = (EditText) findViewById(R.id.portTextBox);
+
+        String ipAddressText = ipAddressTextBox.getText().toString();
+        String portText = portTextBox.getText().toString();
+        Log.d("VALUE", "ipAddressText: " + ipAddressText);
+        Log.d("VALUE", "portText: " + portText);
+        Log.d("INFO", "Saved Values!");
     }
 }
