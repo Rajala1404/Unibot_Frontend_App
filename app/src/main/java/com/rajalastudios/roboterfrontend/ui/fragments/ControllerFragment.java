74a381f49c2fba596833356f4ae2826920f9fd26
Rajala1404 <post.rajan@schirmer-family.de>
2024-05-28 15:17:33 +0200

    Added Joystick Control

--- app/src/main/java/com/rajalastudios/roboterfrontend/ui/fragments/ControllerFragment.java
+++ app/src/main/java/com/rajalastudios/roboterfrontend/ui/fragments/ControllerFragment.java
@@ -8,12 +8,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.rajalastudios.roboterfrontend.MainActivity;
 import com.rajalastudios.roboterfrontend.R;
 import com.rajalastudios.roboterfrontend.ui.JoystickView;
 
-public class ControllerFragment extends Fragment {
+public class ControllerFragment extends Fragment implements View.OnClickListener{
 
     private View view;
     private MainActivity mainActivity;
@@ -23,6 +25,15 @@ public class ControllerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_controller, container, false);
         mainActivity = (MainActivity) getActivity();
+
+        FloatingActionButton rebootButton = view.findViewById(R.id.rebootButton);
+        FloatingActionButton turnLeftButton = view.findViewById(R.id.turnLeftButton);
+        FloatingActionButton turnRightButton = view.findViewById(R.id.turnRightButton);
+
+        rebootButton.setOnClickListener(this);
+        turnLeftButton.setOnClickListener(this);
+        turnRightButton.setOnClickListener(this);
+
 
 
         // Joystick Listener
@@ -45,5 +56,14 @@ public class ControllerFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.turnLeftButton) {
+            mainActivity.sendData("MTL");
+        } else if (view.getId() == R.id.turnRightButton) {
+            mainActivity.sendData("MTR");
+        }
     }
 }
